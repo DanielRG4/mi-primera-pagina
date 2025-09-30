b6e7df9dbd4af643d1e60ab3514a3c31cd85f5dd
DanielRG4 <108098485+DanielRG4@users.noreply.github.com>
2025-09-30 12:33:43 -0600

    mi primera version con html, css, js

---
+++ secuencial.js
@@ -0,0 +1,9 @@
+const precioInicial = 150;
+const descuento = 0.1;
+
+const montoDescuento = precioInicial * descuento;
+const precioFinal = precioInicial - montoDescuento;
+
+
+console.log(`El precio final es: ${precioFinal}`);
+
